7d2af55b8dab6031b8635bcf5f7028d9cccff783
Onel EZIN <ezingodson@gmail.com>
2026-06-04 15:48:43 +0100

    Last before push

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
     e.preventDefault()
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://immaa-backend.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
